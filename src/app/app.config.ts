3380372b5834760463e604ddcd536d94b830533e
Nicolas Soares Henrique <nicolas.soaresh.98@gmail.com>
2025-05-21 15:03:55 -0300

    removed zone.js

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,17 @@
-import {ApplicationConfig} from '@angular/core';
-import {provideRouter} from '@angular/router';
-
-import {routes} from './app.routes';
-import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withFetch, withInterceptors } from "@angular/common/http";
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(
-      withFetch()
-    )
-  ]
+    provideHttpClient(withFetch()),
+    provideExperimentalZonelessChangeDetection(),
+  ],
 };
